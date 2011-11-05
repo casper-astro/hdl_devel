b9864f2ebe2a3ca97b6081fa0b5fcbe691a2a546
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-11-05 17:19:29 -0400

    Added a few more tests to the 'test_counter.py'.
    This required renaming the 'setUp' and 'tearDown' methods of the
    class to 'setUpClass' and 'tearDownClass' respectively. These classes
    are called only when creating the class and not before and after every
    test. This was needed since there can only be one Cosimulation object
    open at a time.

--- libraries/primitives/counter/test_counter.py
+++ libraries/primitives/counter/test_counter.py
@@ -37,33 +37,26 @@ def counter(clk, en, rst, out,
 
 class TestCounterProperties(TestCase):
 
-    def setUp(self):
-        self.architecture = 'BEHAVIORAL'
-        self.data_width = 8
-        self.count_from = 0
-        self.count_to = 255
-        self.step = 1
+    @classmethod
+    def setUpClass(cls):
+        """ Generating the Cosimulation instance """
+        cls.architecture = 'BEHAVIORAL'
+        cls.data_width = 8
+        cls.count_from = 0
+        cls.count_to = 255
+        cls.step = 1
 
-        self.clk = Signal(intbv(0))
-        self.en = Signal(intbv(0))
-        self.rst = Signal(intbv(0))
-        self.out = Signal(intbv(0))
+        cls.clk = Signal(intbv(0))
+        cls.en = Signal(intbv(0))
+        cls.rst = Signal(intbv(0))
+        cls.out = Signal(intbv(0))
 
-        self.dut = counter(self.clk, self.en, self.rst, self.out, 
-                           self.architecture, self.data_width, 
-                           self.count_from, self.count_to, self.step)
+        cls.dut = counter(cls.clk, cls.en, cls.rst, cls.out, 
+                          cls.architecture, cls.data_width, 
+                          cls.count_from, cls.count_to, cls.step)
+        cls.output = []
 
-    def test_output(self):
-        """ Check that the counter increments """
-
-        output = []
-
-        def test(clk, en, rst, out, 
-                 architecture=self.architecture,
-                 data_width=self.data_width,
-                 count_from=self.count_from,
-                 count_to=self.count_to,
-                 step=self.step):
+        def test(clk, en, rst, out):
             
             # set initial values
             @instance
@@ -82,26 +75,32 @@ class TestCounterProperties(TestCase):
             def monitor():
                 while True:
                     yield clk.posedge
-                    output.append(int(out))
+                    cls.output.append(int(out))
 
-            # stop the simulation after
-            # certain number of clocks
-            @instance
-            def stop_sim():
-                while True:
-                    yield clk.posedge
-                    if out == count_to:
-                        raise StopSimulation
+            return initial, drive_clk, monitor
 
-            return initial, drive_clk, monitor, stop_sim
+        check =  test(cls.clk, cls.en, cls.rst, cls.out)
+        sim = Simulation(cls.dut, check)
+        sim.run(1024, quiet=True)
+        cls.latency = cls.output.index(cls.count_from+cls.step) - 1
+        cls.expected = [0]*cls.latency + range(cls.count_from, cls.count_to, cls.step)
 
-        check =  test(self.clk, self.en, self.rst, self.out)
-        sim = Simulation(self.dut, check)
-        sim.run(quiet=True)
-        self.assertEqual(output, [0]+range(self.count_to+1))
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         os.remove('{mod_path}/counter_tb'.format(mod_path=MOD_PATH))
+
+    def test_output_latency(self):
+        """ Check output latency is within specification """
+        self.assertLessEqual(self.latency, 3)
+
+    def test_full_range(self):
+        """ Check full output range for correctness """
+        self.assertEqual(self.output[:len(self.expected)], self.expected)
+
+    def test_roll_over(self):
+        """ Check roll-over at COUNT_TO """
+        last = self.output.index(self.count_to)
+        self.assertEqual(self.output[last+1], self.count_from)
 
 
 if __name__ == "__main__":
