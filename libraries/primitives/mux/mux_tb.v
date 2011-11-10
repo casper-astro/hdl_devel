caf587323db40d5d34220c8a1421d9aae023a07d
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-11-09 22:03:11 -0500

    The mux Verilog test-bench can now be run by a MyHDL co-simulator.
    Macros were added for passing parameters to the Icarus compiler
    through MyHDL when instantiating a Cosimulation instance. See the
    counter primitive for an example.
    
    This relates to GH-2.

--- libraries/primitives/mux/mux_tb.v
+++ libraries/primitives/mux/mux_tb.v
@@ -1,43 +1,54 @@
 // mux test-bench script
 
-`include "mux.v"
-
 module mux_tb;
 
    // local parameters
-   localparam SELECT_LINES = 8;
+   localparam LOCAL_SELECT_LINES = `ifdef SELECT_LINES `SELECT_LINES `else 2 `endif;
 
    // declare regs
-   reg [SELECT_LINES-1:0]select;
-   reg [(2**SELECT_LINES)-1:0]in;
+   reg [LOCAL_SELECT_LINES-1:0] select;
+   reg [(2**LOCAL_SELECT_LINES)-1:0] in;
    
    // declare wires
    wire out; 
 
-   mux
-     #(
-       .ARCHITECTURE("BEHAVIORAL"),
-       .SELECT_LINES(SELECT_LINES)
-       ) mux_inst
-       (
-	.select(select), 
-	.in(in), 
-	.out(out)
-       );
+   // instance, "(d)esign (u)nder (t)est"
+   mux dut (
+	    .select(select), 
+	    .in(in), 
+	    .out(out)
+	    );
+
+   // define all of its parameters
+   defparam dut.ARCHITECTURE = `ifdef ARCHITECTURE `ARCHITECTURE `else "BEHAVIORAL" `endif;
+   defparam dut.SELECT_LINES = `ifdef SELECT_LINES `SELECT_LINES `else 2            `endif;
+
+`ifdef MYHDL
+      
+   // define what myhdl takes over
+   // only if we're running myhdl   
+   initial begin
+      $from_myhdl(select, in);
+      $to_myhdl(out);
+   end
+
+`else
 
    // initial
    initial
      begin
-	select= 1;
-	in = 0;
-        //out = 0;
+	select= 0;
+	in = 2;
+	$display(out);
      end
 
-   always #10 
+   // set select differently
+   initial #10 
      begin
-        select = 3;
-        in = 0;
+        select = 1;
         $display(out);
      end
 
+`endif
+   
 endmodule
